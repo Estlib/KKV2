dd68051db2588d6d1c6c8663513c09ff37a5ca69
Estlib <75094683+Estlib@users.noreply.github.com>
2026-09-24 13:38:45 +0300

    Instantiating most account views, no content yet

---
+++ KeelteKoolV2/Controllers/AccountsController.cs
@@ -0,0 +1,15 @@
+﻿using KeelteKoolV2.Core.Domain;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+
+namespace KeelteKoolV2.Controllers
+{
+    public class AccountsController : Controller
+    {
+        private readonly UserManager<ApplicationUser> _
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
